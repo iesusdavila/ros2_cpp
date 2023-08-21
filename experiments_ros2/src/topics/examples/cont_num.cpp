8868590e6c9470fa2e57c00c229b631541c5dbcf
Iesus Davila <iesusdavila@gmail.com>
2023-08-21 17:12:42 -0500

    Pub/Sub en un mismo nodo

---
+++ experiments_ros2/src/topics/examples/cont_num.cpp
@@ -0,0 +1,29 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32.hpp>
+
+class SubscriberNode : public rclcpp::Node {
+    public:
+        SubscriberNode() : Node("number_counter") {
+            subscription_ = this->create_subscription<std_msgs::msg::Int32>("number", 10, std::bind(&SubscriberNode::callback, this, std::placeholders::_1));
+            //publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+            publisher_ = this->create_publisher<std_msgs::msg::Int32>("number_count", 10);
+        }
+
+    private:
+        void callback(const std_msgs::msg::Int32::SharedPtr msg) {
+            auto sum_msg = std::make_unique<std_msgs::msg::Int32>();
+            sum_msg->data = msg->data + 2;
+            RCLCPP_INFO(this->get_logger(), "Received: %d, Publishing Sum: %d", msg->data, sum_msg->data);
+            publisher_->publish(std::move(sum_msg));
+        }
+
+        rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+};
+
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SubscriberNode>());
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ experiments_ros2/src/topics/examples/pub_numero.cpp
@@ -0,0 +1,30 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32.hpp>
+
+class PublisherNode : public rclcpp::Node {
+    public:
+        PublisherNode() : Node("number_publisher") {
+            publisher_ = this->create_publisher<std_msgs::msg::Int32>("number", 10);
+            timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&PublisherNode::publishNumber, this));
+        }
+
+    private:
+        void publishNumber() {
+            auto msg = std::make_unique<std_msgs::msg::Int32>();
+            msg->data = number_;
+            RCLCPP_INFO(this->get_logger(), "Publishing: %d", msg->data);
+            publisher_->publish(std::move(msg));
+            ++number_;
+        }
+
+        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        int number_ = 1;
+};
+
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PublisherNode>());
+    rclcpp::shutdown();
+    return 0;
+}
